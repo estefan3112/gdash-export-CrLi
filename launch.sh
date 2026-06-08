07969279981abbb9f174d4294a6abdc607eb0f5a
estefan3112 <estefan3112@gmail.com>
2026-06-08 22:15:58 +0200

    add missing gtk files

--- launch.sh
+++ launch.sh
@@ -2,5 +2,8 @@
 DIR="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$DIR"
 echo "Loading GDash... please wait..."
+export XDG_DATA_DIRS="$DIR/share"
+export GDK_PIXBUF_MODULEDIR="$DIR/lib/gdk-pixbuf-2.0/2.10.0/loaders"
+export GDK_PIXBUF_MODULE_FILE="$DIR/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
 exec ./MacOS/gdash "$@" \
   > ~/Library/Logs/gdash.log 2>&1
